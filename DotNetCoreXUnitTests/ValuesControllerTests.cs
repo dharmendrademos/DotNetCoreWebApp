d0d8616aa6536d439a657014492f8b6a89ba2d87
Dharmendra <dharmendra.ladi@gmail.com>
2017-02-24 23:19:10 +0530

    Test project added

---
+++ DotNetCoreXUnitTests/ValuesControllerTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace DotNetCoreXUnitTests
+{
+    public class ValuesControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
